a9624fe1f77d478104f1456201efc2055e87ed48
Allen <58612763+Allen-XiaoDeng@users.noreply.github.com>
2022-01-15 10:43:30 +0800

    Update dom.js

--- src/dom.js
+++ src/dom.js
@@ -3,7 +3,21 @@ window.dom = {
     return (scope || document).querySelectorAll(selector);
   },
   style(node, name, value) {
-    node.style[name] = value;
+      // dom.style(div, 'color', 'red')
+    if (arguments.length === 3) {
+      node.style[name] = value;
+    } else if (arguments.length === 2) {
+      // dom.style(div, 'color')
+      if (typeof name === 'string') {
+        return node.style[name];
+        // dom.style(div, {color: 'red'})
+      } else if (name instanceof Object) {
+        const object = name;
+        for (let key in object) { 
+          node.style[key] = object[key];
+        }
+      }
+    }
   },
   each(nodeList, fn) {
     for (let i = 0; i < nodeList.length; i++) {
